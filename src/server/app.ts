3b71b89b85fd179ddbf138c4552d2e693d225e58
holyshared <holy.shared.design@gmail.com>
2021-06-08 16:50:50 +0900

    add callback hook

---
+++ src/server/app.ts
@@ -0,0 +1,54 @@
+import express, { Request, Response } from "express";
+import { urlencoded, json } from "body-parser";
+import { jwt } from 'twilio';
+
+const ChatGrant = jwt.AccessToken.ChatGrant;
+
+const app = express();
+
+app.use(
+  express.static("public", {
+    maxAge: 60,
+  }),
+);
+
+app.use(urlencoded({ extended: false }))
+app.use(json())
+
+app.post("/token", (req: Request, res: Response) => {
+  const chatGrant = new ChatGrant({
+    serviceSid: process.env.TWILIO_SERVICE_SID,
+  });
+
+  const token = new jwt.AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET,
+    { identity: process.env.TWILIO_IDENTITY }
+  );
+
+  token.addGrant(chatGrant);
+
+  res.json({ jwt: token.toJwt() });
+});
+
+app.post("/pre-hook", (req: Request, res: Response) => {
+  console.log("pre-hook ------");
+  console.log(req.body);
+  res.status(200).end();
+});
+
+app.post("/post-hook", (req: Request, res: Response) => {
+  console.log("post-hook ------");
+  console.log(req.body);
+  res.status(200).end();
+});
+
+app.use((err, req: Request, res: Response, next) => {
+  console.error(err.stack || err);
+
+  res.status(503);
+  res.send("Internal server error");
+})
+
+app.listen(process.env.PORT);
