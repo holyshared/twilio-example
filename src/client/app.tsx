f99d3b02a5d74047f5a8f4caf9cf592743f441f6
holyshared <holy.shared.design@gmail.com>
2021-06-20 16:22:23 +0900

    add log

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -6,10 +6,13 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/firebase-messaging-sw.js');
 
   Notification.requestPermission((result) => {
+    console.log("requestPermission");
+    console.log(result);
     if (result !== 'granted') {
       return;
     }
     navigator.serviceWorker.ready.then((registration) => {
+      console.log("serviceWorker ready");
       registration.showNotification('ServiceWorker ready', {
         body: 'ServiceWorker ready',
       });
